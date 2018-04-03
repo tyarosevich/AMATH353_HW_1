334f7f9957b1d85b3feeaa7d4df6ae8c585aa51f
tyarosevich <tyranosevich@gmail.com>
2018-04-03 16:34:13 -0700

    Push 4/3

--- yarosevich_hw_1.m
+++ yarosevich_hw_1.m
@@ -2,13 +2,148 @@ clc; clear all; close all;
 
 % 1.a.)
 
+c = -1;
+t = 1:5;
+x = -100:.1:100;
+u_xt = @(x, t) exp(-(x - c * t).^2);
+
+f_list = zeros(length(t), length(x));
+f_list2 = zeros(length(t), length(x));
+
+for n = 1:length(t)
+    f_list(n, :) = u_xt(x, t(n));
+end
+
+%% 1.a.) plots
+set(gcf,'color','w');
+
+p1 = subplot(1,3,1)
+plot(x, f_list(1,:))
+xlim([-7 2])
+ylim([-.2 1.2])
+title('$t = 1$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,2)
+plot(x, f_list(3,:))
+xlim([-7 2])
+ylim([-.2 1.2])
+title('$t = 3$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,3)
+plot(x, f_list(5,:))
+xlim([-7 2])
+ylim([-.2 1.2])
+title('$t = 5$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+export_fig plot_1a.pdf
+
+
+%%
+clc; clear all; close all;
+
+% 1.b.)
+
+c = 10;
+t = 1:5;
+x = -100:.1:100;
+u_xt = @(x, t) exp(-(x - c * t).^2);
+
+
+f_list = zeros(length(t), length(x));
+f_list2 = zeros(length(t), length(x));
+
+for n = 1:length(t)
+    f_list(n, :) = u_xt(x, t(n));
+end
+
+%% 1.b.) plots
+
+set(gcf,'color','w');
+
+p1 = subplot(1,3,1)
+plot(x, f_list(1,:))
+xlim([5 15])
+ylim([-.2 1.2])
+title('$t = 1$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,2)
+plot(x, f_list(2,:))
+xlim([15 25])
+ylim([-.2 1.2])
+title('$t = 2$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,3)
+plot(x, f_list(3,:))
+xlim([25 35])
+ylim([-.2 1.2])
+title('$t = 3$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+export_fig plot_1b.pdf
+
+
+%%
+clc; clear all; close all;
+
+% 1.c.)
+
 c = 1;
 t = 1:5;
 x = -100:.1:100;
-%u_xt = @(x, t) exp(-(x - c * t).^2); 
- 
-%u_xt2 = @(x, t) exp(-(x - c * t^2).^2); % part c.)
+u_xt = @(x, t) exp(-(x - c * t^2).^2); 
 
+
+f_list = zeros(length(t), length(x));
+f_list2 = zeros(length(t), length(x));
+
+for n = 1:length(t)
+    f_list(n, :) = u_xt(x, t(n));
+end
+
+%% 1.c.) plots
+set(gcf,'color','w');
+
+p1 = subplot(1,3,1)
+plot(x, f_list(1,:))
+xlim([-2 4])
+ylim([-.2 1.2])
+title('$t = 1$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,2)
+plot(x, f_list(2,:))
+xlim([0 8])
+ylim([-.2 1.2])
+title('$t = 2$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,3)
+plot(x, f_list(3,:))
+xlim([6 12])
+ylim([-.2 1.2])
+title('$t = 3$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+export_fig plot_1c.pdf
+%%
+clc; clear all; close all;
+
+% 1.d.)
+
+c = 1;
+t = 1:5;
+x = -100:.1:100;
 u_xt = @(x, t) (1/t) * exp(-(x - c * t).^2);  % part d.)
 
 f_list = zeros(length(t), length(x));
@@ -18,20 +153,34 @@ for n = 1:length(t)
     f_list(n, :) = u_xt(x, t(n));
 end
 
-%%
+%% 1.d.) plots
 
-for n = 1:5
-   plot(x, f_list(n, :))
-   xlim([-5 50])
-   ylim([-.2 1.2])
-   pause(1) 
-   
-end
+set(gcf,'color','w');
 
-[M, I] = max(f_list(4, :));
-[M2, I2] = max(f_list(5, :));
-speed = x(I2) - x(I);
-speed
+p1 = subplot(1,3,1)
+plot(x, f_list(1,:))
+xlim([-2 4])
+ylim([-.2 1.2])
+title('$t = 1$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,2)
+plot(x, f_list(2,:))
+xlim([-1 5])
+ylim([-.2 1.2])
+title('$t = 2$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+
+p2 = subplot(1,3,3)
+plot(x, f_list(3,:))
+xlim([0 6])
+ylim([-.2 1.2])
+title('$t = 3$', 'FontSize', 12, 'fontweight', 'bold', 'interpreter', 'latex')
+ylabel('$u(x, t_{k})$', 'FontSize', 14, 'interpreter', 'latex')
+
+export_fig plot_1d.pdf
 %%
 % 3.)
 clc; clear all; close all;
